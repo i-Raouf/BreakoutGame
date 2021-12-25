d07632a24d8a0c970be6f05fad4b04705f127688
Raouf <iraouf86@gmail.com>
2021-12-25 11:52:30 +0100

    detecting collision ball paddle

--- BreakoutGame/MainWindow.xaml.cs
+++ BreakoutGame/MainWindow.xaml.cs
@@ -13,6 +13,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Diagnostics;
 
 namespace BreakoutGame
 {
@@ -35,9 +36,10 @@ namespace BreakoutGame
             // center paddle
             var centerPos = (this.Width - Paddle.Width) / 2;
             MovePaddle(centerPos);
+            Canvas.SetTop(Paddle, (this.Height - 100));
             // position the ball
             Canvas.SetLeft(Ball, ((this.Width - Ball.Width) / 2));
-            Canvas.SetTop(Ball, 400);
+            Canvas.SetTop(Ball, 300);
             // set game timer
             gameTimer.Interval = new TimeSpan(0,0,0,0,1000/60); // 60fps
             gameTimer.Tick +=  new EventHandler(GameTimer_Tick);
@@ -46,14 +48,24 @@ namespace BreakoutGame
 
         private void GameTimer_Tick(object sender, EventArgs e)
         {
-            var ballXpostion = (double)Ball.GetValue(Canvas.LeftProperty);
-            var ballYpostion = (double)Ball.GetValue(Canvas.TopProperty);
+            // moving ball
+            var ballXpostion = Canvas.GetLeft(Ball);
+            var ballYpostion = Canvas.GetTop(Ball);
 
             if (ballXpostion < 0 || ballXpostion > (this.Width - Ball.Width - 15))
                 ballDX = -ballDX;
 
-            if (ballYpostion < 0 || ballYpostion > (this.Height - Ball.Height))
+            if (ballYpostion < 0)
                 ballDY = -ballDY;
+
+            // collision with paddle
+            Rect recBall = new Rect(Canvas.GetLeft(Ball), Canvas.GetTop(Ball), Ball.Width, Ball.Height);
+            Rect recPaddle = new Rect(Canvas.GetLeft(Paddle), Canvas.GetTop(Paddle), Paddle.Width, Paddle.Height);
+            
+            if(recBall.IntersectsWith(recPaddle))
+            {
+                ballDY = -ballDY;
+            }
 
             Canvas.SetLeft(Ball, ballXpostion + ballSpeed * ballDX);
             Canvas.SetTop(Ball, ballYpostion + ballSpeed * ballDY);
